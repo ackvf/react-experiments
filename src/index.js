ae53fa17a203ff24c5b2f498680c990e9410e571
Vítězslav Ackermann <vitezslavferko@gmail.com>
2018-04-27 18:51:06 +0200

    OVERRIDE project settings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function render(Component) {
+  ReactDOM.render(
+    <Component/>,
+    document.getElementById('root')
+  )
+}
+
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+    render(NextApp)
+  })
+}
+
+registerServiceWorker()
